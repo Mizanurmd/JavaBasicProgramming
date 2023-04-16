80412eea73bbeb2eae47207deb1010eff3d4f6a1
Mizanurmd <mizanur021991@gmail.com>
2023-04-16 22:43:20 +0600

    Startprint

---
+++ src/basicJavaPrograming/programmer1/StartPrint.java
@@ -0,0 +1,15 @@
+package basicJavaPrograming.programmer1;
+
+public class StartPrint {
+	public static void main(String[]args) {
+		
+		for(int i =6; i>0; i--) {
+			for(int j =i+1; j<=6; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
